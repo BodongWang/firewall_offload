d0f20a340fed41523844c9a483756fdb90affccd
Bodong Wang <bodong@mellanox.com>
2021-03-12 19:34:18 +0000

    Add source only option
    Signed-off-by: Bodong Wang <bodong@mellanox.com>

--- build.sh
+++ build.sh
@@ -7,19 +7,34 @@ if (( NCPUS < 4 )); then
 	NCPUS=4
 fi
 ROOT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SOURCE_ONLY="n"
 
 # Useful constants
 COLOR_RED="\033[0;31m"
 COLOR_GREEN="\033[0;32m"
 COLOR_OFF="\033[0m"
 
-while getopts ":hp:i:m" arg; do
+usage()
+{
+cat 1>&2 <<EOF
+Usage ${0##*/} [-h|?]
+    -p BUILD_DIR:     Build directiory, defautl build
+    -i INSTALL_DIR:   Install dirctory for GRPC, default build/deps
+    -s:               Download source files only without compiling
+    -h|?             Show this help message
+EOF
+}
+
+while getopts ":hp:i:s:" arg; do
 	case $arg in
 		p)
 			BUILD_DIR="${OPTARG}"
 			;;
 		i)
 			INSTALL_DIR="${OPTARG}"
+			;;
+		s)
+			SOURCE_ONLY="y"
 			;;
 		h | *) # Display help.
 			usage
@@ -50,7 +65,7 @@ if [ -n "$FORCE_INSTALL" ]; then
 	rm -rf "$BUILD_DIR"
 fi
 
-get_grpc() {
+build_grpc() {
 	if [ -f "${DEPS_DIR}/grpc_installed" ]; then
 		echo -e "${COLOR_GREEN}grpc is installed ${COLOR_OFF}"
 		return
@@ -64,6 +79,10 @@ get_grpc() {
 	# pin specific release of grpc to avoid build failures
 	# with new changes in grpc/absl
 	git clone --recurse-submodules -b v1.30.0 https://github.com/grpc/grpc
+	if [[ "$SOURCE_ONLY" == "y" ]]; then
+		echo -e "${COLOR_GREEN}GRPC source download only ${COLOR_OFF}"
+		return
+	fi
 	# this is to deal with a nested dir
 	cd grpc
 	mkdir -p "$GRPC_BUILD_DIR"
@@ -90,31 +109,29 @@ build_session_offload() {
 	pushd .
 	cd "$DEPS_DIR"
 	git clone -b v1beta1 https://github.com/att/sessionOffload.git
+	if [[ "$SOURCE_ONLY" == "y" ]]; then
+		echo -e "${COLOR_GREEN}session offload source download only ${COLOR_OFF}"
+		return
+	fi
 	cd sessionOffload/openoffload/cpp/framework
 	make -j "$NCPUS" server
 	if [ 0 -ne $? ] ; then exit 1 ; fi
-	cp lib/libopof_server.a $ROOT_DIR/lib
+	yes | cp lib/libopof_server.a $ROOT_DIR/lib
 	touch "${DEPS_DIR}/sessionOffload_installed"
 	echo -e "${COLOR_GREEN}sessionOffload is installed ${COLOR_OFF}"
 	popd
 }
 
 build_firewall_offload() {
+	if [[ "$SOURCE_ONLY" == "y" ]]; then
+		return
+	fi
 	cd "$ROOT_DIR"
 	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/mellanox/dpdk/lib/aarch64-linux-gnu/pkgconfig/
 	make -j "$NCPUS"
 	ln -sf ./build/nv_opof
 	if [ 0 -ne $? ] ; then exit 1 ; fi
 	echo -e "${COLOR_GREEN}firewall offload is compiled ${COLOR_OFF}"
-}
-
-create_hugepages() {
-	test -x /dev/hugepages && \
-	echo -e "${COLOR_GREEN}reserved 2G hugepages ${COLOR_OFF}"; return
-	mkdir -p /dev/hugepages
-	mountpoint -q /dev/hugepages || mount -t hugetlbfs nodev /dev/hugepages
-	echo 2048 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-	echo -e "${COLOR_GREEN}reserved 2G hugepages ${COLOR_OFF}"
 }
 
 build_kernel() {
@@ -126,8 +143,6 @@ build_kernel() {
 	echo -e "${COLOR_GREEN}driver is updated ${COLOR_OFF}"
 }
 
-get_grpc
+build_grpc
 build_session_offload
 build_firewall_offload
-create_hugepages
-build_kernel
